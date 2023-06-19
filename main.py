53dc28ea71d2864cf584e2bbe36bc740f1ed2d4c
NRSCARLET <107818695+NRSCARLET@users.noreply.github.com>
2023-06-19 23:33:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""import tkinter as tk
+
+window = tk.Tk()
+window.title("Hello world")
+window.geometry("300x300")
+
+hello = tk.Label(text="peepeepoopoo!")
+hello.pack()
+button = tk.Button(text="Click me!")
+button.pack()
+
+tk.mainloop()"""
+
+print("hello")
